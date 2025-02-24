5ff49d25b03b42ce9ba59221dc24ad1ec7abd52d
tony-darco <tonydarco40@gmail.com>
2025-02-24 14:17:32 -0500

    voice to text added along with openai response

--- assis_txt.py
+++ assis_txt.py
@@ -1,12 +1,20 @@
 import sys
+import os
+from dotenv import load_dotenv
 import pyaudio
 from pynput import keyboard
 import wave
 
+import whisper
+
+from langchain_openai import OpenAI
+from langchain_core.prompts import ChatPromptTemplate
 
 class Assistant:
 
     def __init__(self):
+        load_dotenv()
+        os.environ.get("OPENAI_API_KEY")
         self.FORMAT = pyaudio.paInt16  
         self.CHANNELS = 1             
         self.RATE = 44100              
@@ -26,7 +34,7 @@ class Assistant:
         self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_rel)
         self.listener.start()
 
-        self.main()
+        self.record()
 
     def get_stream_status(self):
         print(f"is it active: {self.stream.is_active()} \n is it stopped: {self.stream.is_stopped()}")
@@ -49,10 +57,39 @@ class Assistant:
                 print("Recording stopped.")
                 self.recording = False
                 self.save_audio()
+                self.cart()
         except AttributeError:
             pass  
 
-    def main(self):
+    def save_audio(self):
+        if self.frames:
+            print("Saving recorded audio...")
+            with wave.open(self.WAVE_OUTPUT_FILENAME, 'wb') as wf:
+                wf.setnchannels(self.CHANNELS)
+                wf.setsampwidth(self.audio.get_sample_size(self.FORMAT))
+                wf.setframerate(self.RATE)
+                wf.writeframes(b''.join(self.frames))
+            print(f"Audio saved as {self.WAVE_OUTPUT_FILENAME}")
+
+    def cart(self):
+        print("working on a response...")
+        llm = OpenAI()
+
+        model = whisper.load_model("turbo")
+        result = model.transcribe("output.wav", fp16=False)
+
+        user_text = result["text"]
+        print(user_text)
+        try:
+            prompt = ChatPromptTemplate.from_template("The User is going to ask you to generate code. do your best to write the python code that meet the requirements. {message}")
+            chain = prompt | llm
+
+            resp = chain.invoke({"message":user_text})
+            print("Here is my response...",resp)
+        except Exception as e:
+            print(e)
+
+    def record(self):
         try:
             while True:
                 if self.recording:
@@ -64,16 +101,6 @@ class Assistant:
         self.stream.stop_stream()
         self.stream.close()
         self.audio.terminate()
-
-    def save_audio(self):
-        if self.frames:
-            print("Saving recorded audio...")
-            with wave.open(self.WAVE_OUTPUT_FILENAME, 'wb') as wf:
-                wf.setnchannels(self.CHANNELS)
-                wf.setsampwidth(self.audio.get_sample_size(self.FORMAT))
-                wf.setframerate(self.RATE)
-                wf.writeframes(b''.join(self.frames))
-            print(f"Audio saved as {self.WAVE_OUTPUT_FILENAME}")
 
 
 start = Assistant()
